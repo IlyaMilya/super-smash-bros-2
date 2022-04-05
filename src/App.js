4fc785a4f5dfba77ac3f31e0deb7d94b4ee29047
Ilya <milyavskyfamily@gmail.com>
2022-04-05 15:06:55 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import Fighter from './components/Fighter';
 
 function App() {
   return (
-    <div className="App">
+    <div className={"App"}>
       <h1>Fighters</h1>
-       <div className= "fighters-grid">
+       <div className={"fighters-grid"}>
          <Fighter name={"Roy"} color={'red'} img={''} />
          <Fighter name={"Fox"} color={'green'} img={''}/>
          <Fighter name={'Mario'} color={'purple'} img={''}/>
-       
     </div>
   </div>
   
--- src/components/Fighter.js
+++ src/components/Fighter.js
@@ -1,6 +1,6 @@
 export default ({name, color}) => {
     return( 
-        <div className="fighter" style={{backgroundColor: color, backgroundImage: `https://www.smashbros.com/assets_v2/img/fighter/${name.tolowerCase()}/main.png`}}>
+        <div className="fighter" style={{backgroundColor: color, backgroundImage: `https://www.smashbros.com/assets_v2/img/fighter/${name.toLowerCase()}/main.png`}}>
             <h4>{name}</h4>
         </div>
     )  
